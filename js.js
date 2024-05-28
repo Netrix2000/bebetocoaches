46aabee48b3a75862dad2a6e8a7761c36c5a4443
papa@bear <88621443+Netrix2000@users.noreply.github.com>
2024-05-28 16:11:55 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,79 @@
+console.log("Hello, wrld!"); // -> Declared Syntax
+
+let height=200;
+let anotherheight; height
+weight=160;
+console.log(height); // -> 200
+console.log(anotherheight); // -> 200
+
+let steps = 100;
+console.log(steps); // -> 100
+steps = 120; // -> 120
+console.log(steps);
+steps = steps + 200;
+console.log(steps); // -> 320
+
+function myfunctino() {
+    console.log("hello wrld");
+    console.log("its hacker 442");
+}
+
+console.log("nicet  meeting u");
+myfunctino();
+
+
+
+// there are many possible and correct variable names
+let rosePrice = 8;
+let lilyPrice = 10;
+let tulipPrice = 2;
+
+let numberOfRoses = 70;
+let numberOfLilies = 50;
+let numberOfTulips = 120;
+
+let rosesValue = rosePrice * numberOfRoses;
+let liliesValue = lilyPrice * numberOfLilies;
+let tulipsValue = tulipPrice * numberOfTulips;
+
+let total = rosesValue + liliesValue + tulipsValue;
+console.log("Rose – unit price:", rosePrice, ", quantity:", numberOfRoses, ", value:", rosesValue);
+console.log("Lily – unit price:", lilyPrice, ", quantity:", numberOfLilies, ", value:", liliesValue);
+console.log("Tulip – unit price:", tulipPrice, ", quantity:", numberOfTulips, ", value:", tulipsValue);
+console.log("Total: ", total);// there are many possible and correct variable names
+
+
+
+
+const rosePrice = 8;
+const lilyPrice = 10;
+const tulipPrice = 2;
+
+let numberOfRoses = 70;
+let numberOfLilies = 50;
+let numberOfTulips = 120;
+
+let rosesValue = rosePrice * numberOfRoses;
+let liliesValue = lilyPrice * numberOfLilies;
+let tulipsValue = tulipPrice * numberOfTulips;
+
+let total = rosesValue + liliesValue + tulipsValue;
+
+console.log("Rose – unit price:", rosePrice, ", quantity:", numberOfRoses, ", value:", rosesValue);
+console.log("Lily – unit price:", lilyPrice, ", quantity:", numberOfLilies, ", value:", liliesValue);
+console.log("Tulip – unit price:", tulipPrice, ", quantity:", numberOfTulips, ", value:", tulipsValue);
+console.log("Total: ", total);
+
+numberOfRoses = numberOfRoses - 20;
+numberOfLilies = numberOfLilies - 30;
+
+rosesValue = rosePrice * numberOfRoses;
+liliesValue = lilyPrice * numberOfLilies;
+tulipsValue = tulipPrice * numberOfTulips;
+
+total = rosesValue + liliesValue + tulipsValue;
+
+console.log("Rose – unit price:", rosePrice, ", quantity:", numberOfRoses, ", value:", rosesValue);
+console.log("Lily – unit price:", lilyPrice, ", quantity:", numberOfLilies, ", value:", liliesValue);
+console.log("Tulip – unit price:", tulipPrice, ", quantity:", numberOfTulips, ", value:", tulipsValue);
+console.log("Total: ", total);
